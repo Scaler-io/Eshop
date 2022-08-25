a6a556c1f7a5833a4614b3c7a148445fbea19d23
sayan95 <sharthakmallik@gmail.com>
2022-08-26 00:47:00 +0530

    - Mongo db infra settings added

---
+++ src/Services/Infrastructure/Eshop.Infrastructure/Mongo/Extension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace Eshop.Infrastructure.Mongo
+{
+    public static class Extension
+    {
+        public static void  AddMongoDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            var configSection = configuration.GetSection("mongo");
+            var mongoConfig = new MongoConfig();
+            configSection.Bind(mongoConfig);
+
+            services.AddSingleton<IMongoClient>(client =>
+            {
+                return new MongoClient(mongoConfig.ConnectionString);
+            });
+
+            services.AddSingleton<IMongoDatabase>(client =>
+            {
+                var mongoClient = client.GetService<MongoClient>();
+                return mongoClient.GetDatabase(mongoConfig.Database);
+            });
+        }
+    }
+}
